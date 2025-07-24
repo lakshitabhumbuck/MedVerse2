f082e58412a376069500e1aaba04b515638b6e63
Lakshita Bhumbuck <122292311+lakshitabhumbuck@users.noreply.github.com>
2025-07-25 03:02:18 +0530

    Update doctorRoute.js

--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -17,11 +17,6 @@ const doctorRouter = express.Router()
 doctorRouter.get('/list', doctorList)
 doctorRouter.post('/login', loginDoctor)
 
-// --- Protected Routes (to be added later) ---
-// doctorRouter.get('/profile', authDoctor, getDoctorProfile)
-// doctorRouter.put('/profile', authDoctor, updateDoctorProfile)
-// doctorRouter.get('/appointments', authDoctor, getDoctorAppointments)
-
 doctorRouter.get('/appointments', authDoctor, appointmentsDoctor)
 doctorRouter.post('/complete-appointment', authDoctor, appointmentComplete)
 doctorRouter.post('/cancel-appointment', authDoctor, appointmentCancel)
